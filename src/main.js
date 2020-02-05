9cebf943f46c2db578724239374b916b7238c562
Your Name <you@example.com>
2020-02-05 16:53:56 +0800

    完成了 router 的配置

--- src/main.js
+++ src/main.js
@@ -1,6 +1,8 @@
 import Vue from 'vue'
 import App from './App.vue'
 
+import router from '@/router'
+
 import ElementUI from 'element-ui'
 import 'element-ui/lib/theme-chalk/index.css'
 
@@ -9,5 +11,7 @@ Vue.use(ElementUI)
 Vue.config.productionTip = false
 
 new Vue({
-  render: h => h(App)
+  render: h => h(App),
+  router
+
 }).$mount('#app')
