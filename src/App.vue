8d1a07708b10e3019bcd13f855484e544416c74b
Your Name <you@example.com>
2020-02-06 11:54:57 +0800

    登录功能初始化

--- src/App.vue
+++ src/App.vue
@@ -1,8 +1,6 @@
 <template>
   <div id="app">
-    <el-button type="success">成功按钮</el-button>
-    <el-button size="small">评论</el-button>
-    <el-button type="danger" icon="el-icon-delete" circle></el-button>
+   <router-view></router-view>
   </div>
 </template>
 
---
+++ src/views/login/index.vue
@@ -0,0 +1,38 @@
+<template>
+  <div class="container">
+    <el-card class="my-card">
+      <img src="../../assets/logo_index.png" alt />
+    </el-card>
+  </div>
+</template>
+
+<script>
+export default {
+  name: "app-login"
+};
+</script>
+
+<style scoped lang='less'>
+.container {
+  width: 100%;
+  height: 100%;
+  position: absolute;
+  top: 0;
+  left: 0;
+  background: url(../../assets/login_bg.jpg) no-repeat;
+  background-size: cover;
+  .my-card {
+    width: 400px;
+    height: 350px;
+    transform: translate(-50%, -60%);
+    position: absolute;
+    left: 50%;
+    top: 50%;
+    img {
+      width: 200px;
+      display: block;
+      margin: 0 auto 20px;
+    }
+  }
+}
+</style>
