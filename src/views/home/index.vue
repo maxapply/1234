adab1bd82a5a49f0ec752a649fcefb7b120482e0
Your Name <you@example.com>
2020-02-07 10:52:40 +0800

    home 初始化完成

--- src/views/home/index.vue
+++ src/views/home/index.vue
@@ -1,5 +1,11 @@
 <template>
-   <div class='container-home'>123</div>
+     <el-container class='container-home'>
+      <el-aside class="my-aside" width="200px">Aside</el-aside>
+      <el-container>
+        <el-header class="my-header">Header</el-header>
+        <el-main>Main</el-main>
+      </el-container>
+    </el-container>
 </template>
 
 <script>
@@ -9,5 +15,17 @@ export default {
 </script>
 
 <style lang='less'>
+.container-home{
+  width: 100%;
+  height: 100%;
+  position: absolute;
+  top: 0;
+  left: 0;
+  .my-aside{
+    background-color: #002233;
+  }
+  .my-header{
+    border-bottom: 1px solid #ddd;
+  }
+}
 </style>
-
