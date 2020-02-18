f9bfefd70cd9f7250a0c2a5625b4f5980ee6739b
Your Name <you@example.com>
2020-02-18 14:14:37 +0800

    完成了修改文章功能

--- src/views/publish/index.vue
+++ src/views/publish/index.vue
@@ -1,8 +1,8 @@
 <template>
    <div class='container-publish'>
      <el-card class="box-card">
-      <div slot="header" class="clearfix">
-           <my-bread>发布文章</my-bread>
+      <div slot="header" >
+           <my-bread >  {{$route.query.id ? '修改' : '发布'}}文章</my-bread>
       </div>
       <el-form label-width="120px">
         <el-form-item label="标题 :">
@@ -13,24 +13,28 @@
          </el-form-item>
          <el-form-item label="封面 :">
 
-            <el-radio-group v-model="articleFrom.cover.type">
+            <el-radio-group v-model="articleFrom.cover.type" @change="articleFrom.cover.images=[]">
                   <el-radio :label="1">单图</el-radio>
                   <el-radio :label="3">三图</el-radio>
                   <el-radio :label="0">无图</el-radio>
                   <el-radio :label="- 1">自动</el-radio>
             </el-radio-group>
-            <div>
+            <div v-if="articleFrom.cover.type===1">
               <my-image v-model="articleFrom.cover.images[0]"></my-image>
-              <my-image></my-image>
-              <my-image></my-image>
+            </div>
+            <div v-if="articleFrom.cover.type===3">
+              <my-image v-for="i in 3" :key="i" v-model="articleFrom.cover.images[i-1]"></my-image>
             </div>
          </el-form-item>
          <el-form-item label="频道 :">
            <my-channel v-model="articleFrom.channel_id"></my-channel>
          </el-form-item>
-         <el-form-item >
-           <el-button type="primary">发布文章</el-button>
-           <el-button>存入草稿</el-button>
+         <el-form-item v-if="$route.query.id">
+           <el-button @click="updata()" type="success">修改文章</el-button>
+         </el-form-item>
+         <el-form-item v-else>
+           <el-button  @click="submit(false)" type="primary">发布文章</el-button>
+           <el-button @click="submit(true)" >存入草稿</el-button>
          </el-form-item>
       </el-form>
     </el-card>
@@ -46,6 +50,9 @@ import MyImage from '@/components/my-image'
 export default {
   components: { quillEditor, MyImage },
   name: 'app-publish',
+  created () {
+    this.getarticle()
+  },
   data () {
     return {
       articleFrom: {
@@ -71,6 +78,51 @@ export default {
         }
       }
     }
+  },
+  watch: {
+    '$route.query.id': function () {
+      this.toggleFormInfo()
+    }
+  },
+  methods: {
+    toggleFormInfo () {
+      if (this.$route.query.id) {
+        this.getarticle()
+      } else {
+        this.articleFrom = {
+          title: null,
+          channel_id: null,
+          content: null,
+          cover: {
+            type: 1,
+            images: []
+          }
+        }
+      }
+    },
+    async updata () {
+      try {
+        await this.$http.put(`/articles/${this.articleFrom.id}?draft=false`, this.articleFrom)
+        this.$message.success('修改文章成功')
+        this.$router.push('/article')
+      } catch (e) {
+        this.$message.error('操作失败')
+      }
+    },
+    async submit (draft) {
+      try {
+        await this.$http.post(`articles?draft=${draft}`, this.articleFrom)
+        this.$message.success(draft ? '存入草稿成功' : '发布文章成功')
+        this.$router.push('/article')
+      } catch (e) {
+        this.$message.error('操作失败')
+      }
+    },
+    async getarticle () {
+      const res = await this.$http.get(`/articles/${this.$route.query.id}`)
+      this.articleFrom = res.data.data
+    }
+
   }
 }
 </script>
